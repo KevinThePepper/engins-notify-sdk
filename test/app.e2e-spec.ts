1c2be32356888b45884fa0a7c207073600cb23ea
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-10-22 08:58:31 -0700

    Changed from templated hello message to test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/api/v1/test (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/api/v1/test')
       .expect(200)
-      .expect('Hello World!');
+      .expect('test');
   });
 });
