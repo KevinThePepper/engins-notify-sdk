bd8eeac518df38f4e0cbd40d7d02720d39b6a2f6
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-10-22 08:55:23 -0700

    jsdocs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { AppService } from './app.service';
 import { LoggerModule } from './util/logger/logger.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
+/**
+ * Base app module used to import remaining modules, controllers and services.
+ */
 @Module({
   imports: [
     LoggerModule,
@@ -20,6 +23,10 @@ import { LoggerMiddleware } from './middleware/logger.middleware';
   providers: [AppService],
 })
 export class AppModule implements NestModule {
+  /**
+   * Configures middleware for the app.
+   * @param consumer Interface defining method for applying user defined middleware to routes.
+   */
   configure(consumer: MiddlewareConsumer): void {
     // add logger middleware to log all requests
     consumer.apply(LoggerMiddleware).forRoutes({path: '*', method: RequestMethod.ALL});
