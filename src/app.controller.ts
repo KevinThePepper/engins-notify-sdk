2105305464b038db6220adf396f4d1050c8a3a3a
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-10-22 08:54:52 -0700

    Changed to test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller()
+/**
+ * Base app controller.
+ */
+@ApiBearerAuth()
+@ApiTags('app')
+@Controller('/api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  /**
+   * Test endpoint that returns the string `test`.
+   */
+  @Get('test')
+  @ApiOperation({ summary: 'Test endpoint' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully tested',
+  })
+  getTest(): string {
+    return this.appService.getTest();
   }
 }
