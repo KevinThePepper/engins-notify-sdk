55188a1da00cb4253a312c0fa922db9936a8885d
Kevin Shelley <kevin.shelley.dev@pm.me>
2020-10-26 01:10:45 -0700

    Removed V1 naming nomenclature

--- src/config/redoc.config.ts
+++ src/config/redoc.config.ts
@@ -1,10 +1,10 @@
 import { DocumentBuilder } from '@nestjs/swagger';
-import { RedocOptions, RedocTheme } from 'nestjs-redoc';
+import { RedocOptions } from 'nestjs-redoc';
 
 /**
  * API version 1 Swagger documentation.
  */
-export const SWAGGER_OPTIONS_V1 = new DocumentBuilder()
+export const SWAGGER_OPTIONS = new DocumentBuilder()
     .setTitle('ENGINS Notify SDK')
     .setDescription('API-driven templated notifications across the ENGINS platform')
     .setVersion('1.0')
@@ -14,7 +14,7 @@ export const SWAGGER_OPTIONS_V1 = new DocumentBuilder()
 /**
  * API version 1 Redocs options.
  */
-export const REDOC_OPTIONS_V1: RedocOptions = {
+export const REDOC_OPTIONS: RedocOptions = {
     title: 'ENGINS Notify SDK',
     logo: {
         url: 'https://www.enginsociety.org/images/icons/ENGINS%20Expanded%20Logo%20White.png',
